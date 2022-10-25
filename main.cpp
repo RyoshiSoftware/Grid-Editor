7ec9760e75524ed3af0acb47fce0b51db559c28b
Hoshinohrian <90474813+Hoshinohrian@users.noreply.github.com>
2022-10-25 14:09:01 -0500

    Added level map and loaded it into the program

--- main.cpp
+++ main.cpp
@@ -11,11 +11,17 @@ int main()
     Grid testGrid(256, 256);
     testGrid.createCells();
 
+    Texture2D levelTex = LoadTexture("assets/LevelMap.png");
+    float levelPosX = static_cast<float>(windowWidth / 2) - (levelTex.width / 2);
+    float levelPosY = static_cast<float>(windowHeight / 2) - (levelTex.height / 2);
+
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        DrawTexture(levelTex, levelPosX, levelPosY, WHITE);
 
         testGrid.tick();
 
